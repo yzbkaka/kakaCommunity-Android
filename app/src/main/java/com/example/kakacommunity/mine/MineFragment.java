c0409c4ac8687e50067c2540f138c0ffe75c5fa0
yzbkaka <yzbkaka@foxmail.com>
2020-12-14 16:08:34 +0800

    完成我的和我的帖子界面

--- app/src/main/java/com/example/kakacommunity/mine/MineFragment.java
+++ app/src/main/java/com/example/kakacommunity/mine/MineFragment.java
@@ -1,85 +1,76 @@
 package com.example.kakacommunity.mine;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.kakacommunity.MyApplication;
 import com.example.kakacommunity.R;
-import com.example.kakacommunity.home.HomeAdapter;
-import com.example.kakacommunity.model.HomeArticle;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+public class MineFragment extends Fragment implements View.OnClickListener {
 
-public class MineFragment extends Fragment {
+    private LinearLayout myArticel;
 
-    private RecyclerView recyclerView;
+    private LinearLayout web;
 
-    private HomeAdapter homeAdapter;
+    private LinearLayout tree;
 
-    private ImageView backImage;
+    private LinearLayout collect;
 
-    private ImageView headImage;
+    private LinearLayout setting;
 
-    private TextView userName;
+    private LinearLayout about;
 
-    private List<HomeArticle> articleList = new ArrayList<>();
+    private LinearLayout exit;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mine, container, false);
-        recyclerView = (RecyclerView)view.findViewById(R.id.mine_recycler_view);
-        backImage = (ImageView)view.findViewById(R.id.back_image);
-        headImage = (ImageView)view.findViewById(R.id.head_image);
-        userName = (TextView)view.findViewById(R.id.user_name);
-        initRecyclerView();
+        myArticel = (LinearLayout)view.findViewById(R.id.my_article);
+        myArticel.setOnClickListener(this);
+        web = (LinearLayout)view.findViewById(R.id.web);
+        web.setOnClickListener(this);
+        tree = (LinearLayout)view.findViewById(R.id.tree);
+        tree.setOnClickListener(this);
+        collect = (LinearLayout)view.findViewById(R.id.collect);
+        collect.setOnClickListener(this);
+        setting = (LinearLayout)view.findViewById(R.id.setting);
+        setting.setOnClickListener(this);
+        about = (LinearLayout)view.findViewById(R.id.about);
+        about.setOnClickListener(this);
+        exit = (LinearLayout)view.findViewById(R.id.exit);
+        exit.setOnClickListener(this);
         return view;
     }
 
-    private void initRecyclerView() {
-        LinearLayoutManager manager = new LinearLayoutManager(MyApplication.getContext());
-        homeAdapter = new HomeAdapter(articleList);
-        recyclerView.setLayoutManager(manager);
-        recyclerView.setAdapter(homeAdapter);
-    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        getMineArticleJSON();
-        homeAdapter.setOnItemCLickListener(new HomeAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-
-            }
-        });
     }
 
-    private void getMineArticleJSON() {
-        for(int i = 0;i < 20;i++) {
-            HomeArticle homeArticle = new HomeArticle();
-            homeArticle.setAuthor("yzbkaka");
-            homeArticle.setTitle("双非的秋招总结-已拿offer");
-            homeArticle.setNiceDate(new Date().toString());
-            homeArticle.setTag("讨论区");
-            articleList.add(homeArticle);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.my_article:
+                Intent intent = new Intent(MyApplication.getContext(),MyArticleActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.web:
+            case R.id.tree:
+            case R.id.collect:
+            case R.id.setting:
+            case R.id.about:
+            case R.id.exit:
         }
-    }
-
-    private void parseMineArticeJSON() {
-
     }
 }
---
+++ app/src/main/java/com/example/kakacommunity/mine/MyArticleActivity.java
@@ -0,0 +1,92 @@
+package com.example.kakacommunity.mine;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.kakacommunity.MyApplication;
+import com.example.kakacommunity.R;
+import com.example.kakacommunity.home.HomeAdapter;
+import com.example.kakacommunity.model.HomeArticle;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MyArticleActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    private RecyclerView recyclerView;
+
+    private HomeAdapter myArticleAdapter;
+
+    private List<HomeArticle> myArticleList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_article);
+        initView();
+    }
+
+    private void initView() {
+        toolbar = (Toolbar)findViewById(R.id.my_article_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        recyclerView = (RecyclerView)findViewById(R.id.my_article_recycler_view);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        LinearLayoutManager manager = new LinearLayoutManager(MyApplication.getContext());
+        myArticleAdapter = new HomeAdapter(myArticleList);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(myArticleAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getMyArticleJSON();
+        myArticleAdapter.setOnItemCLickListener(new HomeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+
+            }
+        });
+    }
+
+    private void getMyArticleJSON() {
+        for(int i = 0;i < 20;i++) {
+            HomeArticle homeArticle = new HomeArticle();
+            homeArticle.setAuthor("yzbkaka");
+            homeArticle.setTitle("双非的秋招总结-已拿offer");
+            homeArticle.setNiceDate(new Date().toString());
+            homeArticle.setTag("讨论区");
+            myArticleList.add(homeArticle);
+        }
+    }
+
+    private void parseMyArticleJSON(String responseData) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:  //默认id
+                finish();
+                break;
+        }
+        return true;
+    }
+}
