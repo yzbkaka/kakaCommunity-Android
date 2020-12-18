44e3432de49fedf0893c1029f354ae17a6bbc4ff
yzbkaka <yzbkaka@foxmail.com>
2020-12-18 16:20:46 +0800

    完成关于项目

--- app/src/main/java/com/example/kakacommunity/mine/AboutActivity.java
+++ app/src/main/java/com/example/kakacommunity/mine/AboutActivity.java
@@ -1,16 +1,86 @@
 package com.example.kakacommunity.mine;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+
+import com.example.kakacommunity.MyApplication;
 import com.example.kakacommunity.R;
+import com.example.kakacommunity.home.WebActivity;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    private TextView versionText;
+
+    private LinearLayout startLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        initView();
+    }
+
+
+    private void initView(){
+        toolbar = (Toolbar) findViewById(R.id.about_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        versionText = (TextView) findViewById(R.id.about_version);
+        startLayout = (LinearLayout) findViewById(R.id.about_start_ll);
+        versionText.setText(getVersion());
+        startLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyApplication.getContext(), WebActivity.class);
+                intent.putExtra("link", "https://github.com/yzbkaka");
+                intent.putExtra("title", "yzbkaka");
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+        return true;
+    }
+
+
+    /**
+     * 获取app版本号
+     */
+    public String getVersion() {
+        try {
+            PackageManager manager = this.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+            String version = info.versionName;
+            return this.getString(R.string.version_name) + version;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return this.getString(R.string.can_not_find_version_name);
+        }
     }
 }
